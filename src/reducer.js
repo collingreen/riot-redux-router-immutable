1d1a89fba0c6e00269ee96a1c24d91faee3de1e3
Collin Green <collin@collingreen.com>
2017-11-29 19:47:34 -0800

    add previous_url; add currentUrl/previousUrl
    Add previous_url/previousUrl to track the url the router was previously
    on.
    
    Deprecate the snake_case fields in favor of camelCase.

--- src/reducer.js
+++ src/reducer.js
@@ -2,11 +2,24 @@ var Immutable = require('immutable')
 var actions = require('./actions')
 
 module.exports = function(state, action) {
-  state = state || Immutable.fromJS({ current_url: '' })
+  state = state || Immutable.fromJS({
+    current_url: '',
+    previous_url: '',
+    currentUrl: '',
+    previousUrl: ''
+  })
 
   switch(action.type) {
     case actions.ROUTER_ROUTE_CHANGED:
-      return state.merge({current_url: action.data})
+      var currentUrl = action.data
+      var previousUrl = state.get('currentUrl')
+      return state.merge({
+        // snake_case versions deprecated for camelCase
+        current_url: currentUrl,
+        previous_url: previousUrl,
+        currentUrl: currentUrl,
+        previousUrl: previousUrl
+      })
     default:
       return state
   }
